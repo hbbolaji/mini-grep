039e4a8c7e832eb693170656c711c435554051ac
Hbbolaji <bolaji1050@gmail.com>
2026-04-13 20:53:44 +0100

    Add poem and implement command-line argument parsing in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::{ env, error::Error, fs, process };
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = &args[1];
+        let filename = &args[2];
+
+        return Ok(Config { query: String::from(query), filename: String::from(filename) });
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err: &str| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1)
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content =  fs::read_to_string(config.filename)?;
+
+    println!("With text: \n{}", content);
+    Ok(())
 }
